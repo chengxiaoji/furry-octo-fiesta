61b47dc1a64ceabccebf22e55f246a850a86995a
chengxiaoji <2572432678@qq.com>
2022-02-13 22:40:39 +0800

    login

--- src/router/index.js
+++ src/router/index.js
@@ -6,22 +6,37 @@ Vue.use(VueRouter)
 
 const routes = [
   {
+    path:'/login',
+    name:'login',
+    component: ()=> import('@/views/Login/Login')
+  },
+  // 项目启动默认展示的组件
+  {
     path: '/',
+    redirect:'login',
+    name: 'login',
+    component: ()=> import('@/views/Login/Login')
+  },
+  // home组件
+  {
+    path: '/home',
     name: 'Home',
     component: Home
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // }
 ]
 
 const router = new VueRouter({
-  routes
+  routes,
+  mode:'history'
 })
 
 export default router
---
+++ src/views/Login/Login.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="login">
+    <div class="loginBox">
+      <div class="leftBg">
+
+      </div>
+      <div class="rightInfo">
+        <el-card class="box-card">
+        <div slot="header" class="clearfix">
+          <span>登录</span>
+        </div>
+        <div class="form">
+          <el-form ref="form" :model="form" label-width="80px">
+            <el-form-item label="账号:">
+              <el-input v-model="form.account"></el-input>
+            </el-form-item>
+            <el-form-item label="密码:">
+              <el-input v-model="form.password" type="password"></el-input>
+            </el-form-item>
+            <div class="textLink">
+              <el-link type="primary">忘记密码？</el-link>
+              <el-link type="primary">记住密码</el-link>
+            </div>
+
+            <div class="loginBtn">
+              <el-button style="width: 24rem" type="primary" @click="onSubmit">登录</el-button>
+              <el-button style="width: 24rem; margin-top:2rem" @click="onSubmit">注册</el-button>
+            </div>
+          </el-form>
+        </div>
+      </el-card>
+      </div>
+      
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      form: {
+        account: "",
+        password: "",
+        date1: "",
+        date2: "",
+        delivery: false,
+        type: [],
+        resource: "",
+        desc: "",
+      },
+    };
+  },
+  methods: {
+    onSubmit () {
+      console.log("submit!");
+    },
+  },
+};
+</script>
+
+<style  lang="less" scoped>
+.login{
+  background: url('../../assets/img/loginBg.jpg') no-repeat center;
+  // background: black;
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.loginBox {
+  display: flex;
+  justify-content: center;
+  width: fit-content;
+  height: 500px;
+}
+.loginBtn {
+  margin-top: 2rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0 32px;
+}
+
+.textLink {
+  padding: 0 2rem;
+  display: flex;
+  justify-content: space-between;
+}
+
+.clearfix {
+  font-size: 1.5rem;
+  font-weight: bold;
+  text-align: center;
+}
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: "";
+}
+.clearfix:after {
+  clear: both;
+}
+
+.box-card {
+  height: 100%;
+}
+.el-input {
+  width: 90%;
+}
+.leftBg{
+  width: 250px;
+  height: 100%;
+  background: url('../../assets/img/loginDeskLeft.png');
+}
+.el-form-item{
+  margin-top: 36px;
+}
+
+</style>
